e615e431839f813c0f776686bfe153eb0c8a715e
Sheriff Phx <babatundesheriff971@gmail.com>
2026-08-21 16:52:25 +0100

    Resolve build issue

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+
+     // next.config.ts
+   experimental: {
+     turbo: {
+       resolveAlias: {
+         '@x402/evm': false,
+         '@x402/evm/exact/client': false,
+       },
+     },
+   },
 };
+
+
 
 export default nextConfig;
